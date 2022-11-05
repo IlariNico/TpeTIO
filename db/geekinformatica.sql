99373c3dc64c7483f2efdb6a337011d414aa0c41
Valentina Hurtado <vhurtado@alumnos.exa.unicen.edu.ar>
2022-11-04 21:32:03 -0300

    Agrego descripciones en tabla productos de db

--- db/geekinformatica.sql
+++ db/geekinformatica.sql
@@ -77,12 +77,12 @@ CREATE TABLE `productos` (
 
 
 INSERT INTO `productos` (`id`, `nombre`, `precio`, `stock`, `nombreCorto`, `marca`, `descripcion`, `img`) VALUES
-(1, 'ASUS X515EA 15.6 INTEL I3-1115', 118843, 50, 'X515', 'ASUS', 'descripcion 1', '.img/prod1.jpg'),
-(2, 'LENOVO THINKBOOK PANTALLA 15.6', 240933, 30, 'ThinkBook Gen 2 ITL', 'Lenovo', 'descripcion 2', './img/prod2.jpg'),
-(3, 'LENOVO V15 PANTALLA 15,6 CORE ', 209517, 20, 'V15', 'Lenovo', 'descripcion 3', './img/prod3.jpg'),
-(4, 'BANGHO 14\" MAX L4 I5 8GB 240GB', 180843, 50, 'max l4 i5', 'BANGHO', 'descripcion 4', './img/prod4.jpg'),
-(5, 'lenovo thinkpad L15 15,6\" core', 199099, 40, 'thinkpad L15', 'Lenovo', 'descripcion 5', './img/prod5.jpg'),
-(6, 'Acer aspire 3 15.6\" core i5 11', 165890, 20, 'Aspire 3 A315', 'Aspire', 'descripcion 6', './img/prod6.jpg');
+(1, 'ASUS X515EA 15.6 INTEL I3-1115', 118843, 50, 'X515', 'ASUS', 'Ya sea para trabajar o para jugar, ASUS 15 X515, es una computadora portatil que ofrece un potente rendimiento y efectos visuales envolventes.', '.img/prod1.jpg'),
+(2, 'LENOVO THINKBOOK PANTALLA 15.6', 240933, 30, 'ThinkBook Gen 2 ITL', 'Lenovo', 'Con paneles de aluminio, esta laptop presenta una estetica elegante y profesional. Es de las mas delgadas hasta la fecha, lo que las hace facil de transportar y perfectas para el trabajo remoto', './img/prod2.jpg'),
+(3, 'LENOVO V15 PANTALLA 15,6 CORE ', 209517, 20, 'V15', 'Lenovo', 'Esta no es la típica laptop de plástico o metal frío que llevas de un lado a otro: es un accesorio de moda. La IdeaPad 5 se ha diseñado poniendo especial atención en los detalles, lo que queda patente en la suavidad al tacto de la superficie de esta computadora', './img/prod3.jpg'),
+(4, 'BANGHO 14\" MAX L4 I5 8GB 240GB', 180843, 50, 'max l4 i5', 'BANGHO', 'Gracias a su peso ligero y tamaño reducido podrás llevar tus archivos y documentos a todos lados sin perder prestaciones, tiene una pantalla de 14 pulgadas HD para visualización completa y Windows 11.', './img/prod4.jpg'),
+(5, 'lenovo thinkpad L15 15,6\" core', 199099, 40, 'thinkpad L15', 'Lenovo', 'Esta laptop cuenta con pantallas HD y FHD de 15.6” con la opción touch. Si a esto le añades Dolby Audio para mejorar el sonido, podrás disfrutar a lo grande de la transmisión de películas en este equipo, además de escuchar tu música favorita.', './img/prod5.jpg'),
+(6, 'Acer aspire 3 15.6\" core i5 11', 165890, 20, 'Aspire 3 A315', 'Aspire', 'Calidad que entra por los ojos! diseño elegante y superior, pantalla de alta definicion con panel IPS recomendado para creadores de contenido, colores excepcionales y mayores angulos de vision. Recomendada para diseño grafico y fotografia.', './img/prod6.jpg');
 
 --
 -- Índices para tablas volcadas
